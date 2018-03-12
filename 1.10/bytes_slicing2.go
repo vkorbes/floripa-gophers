cf6a91d53b7bc84913266655e0395eb8c59607b4
Ellen Körbes <ellenkorbes@gmail.com>
2018-03-12 20:54:21 -0300

    update

---
+++ 1.10/bytes_slicing2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// START OMIT
+	text := []byte("Boa noite, galera!")
+	fmt.Printf("text:  %s", desc(text))
+
+	hello := text[0:9:9] // HL
+	fmt.Printf("hello: %s", desc(hello))
+
+	hello = append(hello, '#')
+	fmt.Printf("hello: %s", desc(hello))
+	fmt.Printf("text:  %s", desc(text))
+	// END OMIT
+}
+
+func desc(b []byte) string {
+	return fmt.Sprintf("len: %2d | cap: %2d | %q\n", len(b), cap(b), b)
+}
