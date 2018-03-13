a62bbc4cad6395579ea114eedff98b3d55672fc6
Ellen Körbes <ellenkorbes@gmail.com>
2018-03-12 21:00:40 -0300

    update

---
+++ 1.10/flag/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"flag"
+)
+
+func main() {
+	// START OMIT
+	x := flag.Int("x", 0, "Essa explicação é\nbem longa, sacoé?")
+	y := flag.Int("y", 0, "Essa é curtinha")
+	flag.Parse()
+	// END OMIT
+	flag.PrintDefaults()
+	_, _ = x, y
+}
